a90ff79aabb0403d5d9fc68b19f10aa6b97a5f80
radojka11 <andrejtica@gmail.com>
2024-12-25 18:11:59 +0100

    Added line chart

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,13 @@
-import Chart from 'chart.js/auto'
 import Plotly from 'plotly.js-dist-min'
 import { async } from 'regenerator-runtime';
 
-
-async function getData() {
-  const url = "http://127.0.0.1:8000/data";
+async function getData(all) {
+  let url;
+  if(all){
+    url = "http://127.0.0.1:8000/data/all";
+  } else {
+    url = "http://127.0.0.1:8000/data";
+  }
   try {
     const response = await fetch(url);
     if (!response.ok) {
@@ -18,29 +21,7 @@ async function getData() {
   }
 }
 
-
-//var lineChart = new Chart(
-//  document.getElementById('acquisitions'),
-//  {
-//    type: 'line',
-//    data: {
-//      datasets: [
-//        {
-//          label: 'Acquisitions by year',
-//        }
-//      ]
-//    }
-//  }
-//);
-async function updateDiv() {
-  const data = await getData();
-  lineChart.data.datasets[0].data[2] = data.map(val => val.value);
-  lineChart.data.labels = [1, 2, 3, 4, 5, 6];
-  lineChart.update();
-
-}
-
-var data = [
+var data_gauge = [
   {
     value: 0,
     title: { text: "Speed" },
@@ -50,18 +31,46 @@ var data = [
 ];
 
 async function create_gauge() {
-
   var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
-  Plotly.newPlot('myDiv', data, layout);
+  Plotly.newPlot('gaugeDiv', data_gauge, layout);
 }
 
 async function update_gauge() {
-  const mydata = await getData();
-  console.log(mydata)
-  Plotly.restyle('myDiv', { value: [mydata.value] }, 0);
+  const mydata_gauge = await getData(false);
+  console.log(mydata_gauge);
+  Plotly.restyle('gaugeDiv', { value: [mydata_gauge.value] }, 0);
 }
 
-create_gauge(data)
+create_gauge(data_gauge);
 
-setInterval(update_gauge, 2000)
+setInterval(update_gauge, 2000);
+
+//LINE CHART ------------------
+
+var trace1 = {
+  x: [0],
+  y: [0],
+  type: 'lines'
+};
+
+var data_line = [trace1];
+
+async function create_line() {
+  Plotly.newPlot('lineDiv', data_line);
+}
+
+async function update_line() {
+  const mydata_line = await getData(true);
+  const mydata_map_line_value = mydata_line.map((mydata) => mydata.value)
+  const mydata_map_line_time = mydata_line.map((mydata) => mydata.time)
+  console.log(mydata_map_line_value);
+  Plotly.restyle('lineDiv', { y: [mydata_map_line_value] }, 0);
+  Plotly.restyle('lineDiv', { x: [mydata_map_line_time] }, 0);
+}
+
+document.getElementById("line_button").addEventListener("click", function() {
+  update_line()
+});
+
+create_line()
 
