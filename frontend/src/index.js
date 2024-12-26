56ef36c02e205e1da4995b85ef3d96474a031c6b
radojka11 <andrejtica@gmail.com>
2024-12-26 22:59:57 +0100

    Added switching between pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -66,18 +66,17 @@ async function create_gauge(sensor_type, gauge_div) {
   });
 }
 
-//async function update_chart(chart_div) {
-async function update_chart(chart_type) {
+async function update_chart(chart_type, sensor_type) {
   if (chart_type == "gauge") {
     const chart_data = await getData(false);
-    Plotly.restyle('gaugeDivTemp', { value: [chart_data.value] }, 0);
+    Plotly.restyle(sensor_type == "temp" ? 'gaugeDivTemp' : "gaugeDivCo2", { value: [chart_data.value] }, 0);
     return;
   } else if (chart_type == "line") {
     const chart_data = await getData(true);
     const mydata_map_line_value = chart_data.map((mydata) => mydata.value)
     const mydata_map_line_time = chart_data.map((mydata) => mydata.time)
-    Plotly.restyle('lineDiv', { y: [mydata_map_line_value] }, 0);
-    Plotly.restyle('lineDiv', { x: [mydata_map_line_time] }, 0);
+    Plotly.restyle(sensor_type == "temp" ? 'lineDivTemp' : "lineDivCo2", { y: [mydata_map_line_value] }, 0);
+    Plotly.restyle(sensor_type == "temp" ? 'lineDivTemp' : "lineDivCo2", { x: [mydata_map_line_time] }, 0);
     return;
   } else {
     console.error(`Unknown chart type: ${chart_type}`);
@@ -87,34 +86,55 @@ async function update_chart(chart_type) {
 
 //LINE CHART ------------------
 
-async function create_line() {
-  Plotly.newPlot('lineDiv', [{
-    x: [0],
-    y: [0],
-    type: 'lines'
-  }]
-  );
+async function create_line(sensor_type, line_div) {
+  var layout = {
+    title: { text: sensor_type == "temp" ? "Temperature" : "Co2" }
+  };
+  Plotly.newPlot(line_div, [{ x: [0], y: [0], type: 'lines' }], layout);
 }
 
 const clickableLinks = document.querySelectorAll('#sidebar .links a');
 
+let intervallId1;
+let intervallId2;
 clickableLinks.forEach((link) => {
   if (link.textContent == "Dashboard") {
     link.addEventListener("click", (event) => {
-      const gridContainer = document.querySelector('#main-dashboard-content #grid-container');
-      if (!document.getElementById('welcome-title')) {
-        const welcomeDiv = document.createElement('div');
-        welcomeDiv.id = 'welcome-title';
-        welcomeDiv.textContent = 'Welcome to the dashboard';
-        gridContainer.insertBefore(welcomeDiv, gridContainer.firstChild);
-
-        create_gauge("temp", "gaugeDivTemp");
-        //create_gauge("co2", "gaugeDivCo2");
-        setInterval(update_chart.bind(null, "gauge"), 2000);
-        create_line()
-        setInterval(update_chart.bind(null, "line"), 2000)
-      }
+      clearInterval(intervallId1);
+      clearInterval(intervallId2);
+      Plotly.purge("lineDivTemp");
+      Plotly.purge("lineDivCo2");
+      create_gauge("temp", "gaugeDivTemp");
+      create_gauge("co2", "gaugeDivCo2");
+      intervallId1 = setInterval(update_chart.bind(null, "gauge", "temp"), 2000);
+      intervallId2 = setInterval(update_chart.bind(null, "gauge", "co2"), 2000);
     })
+  } else if (link.textContent == "Analytics") {
+    link.addEventListener("click", (event) => {
+      clearInterval(intervallId1);
+      clearInterval(intervallId2);
+      Plotly.purge("gaugeDivTemp");
+      Plotly.purge("gaugeDivCo2");
+      create_line("temp", "lineDivTemp");
+      create_line("co2", "lineDivCo2");
+      intervallId1 = setInterval(update_chart.bind(null, "line", "temp"), 2000);
+      intervallId2 = setInterval(update_chart.bind(null, "line", "co2"), 2000);
+    })
+  } else if (link.textContent == "Alerts") {
+    link.addEventListener("click", (event) => {
+      let alertsDiv = document.getElementById('main-alerts-content');
+      if (!alertsDiv) {
+        alertsDiv = document.createElement('div');
+        alertsDiv.id = 'alerts';
+        document.body.appendChild(alertsDiv); 
+      }
+      const alertDiv = document.createElement('div');
+      alertDiv.textContent = "No alerts yet!";
+      alertDiv.classList.add('alert-message');
+
+      // Append the new alert message to the alerts container
+      alertsDiv.appendChild(alertDiv);
+    });
   }
 });
 
